fba7ac660999c5cb0486cdedbcd28abc753da19c
Kamil Pomykała <37307597+Akasiek@users.noreply.github.com>
2022-06-08 18:48:14 +0200

    Updated error page

--- frontend/views/site/error.php
+++ frontend/views/site/error.php
@@ -3,17 +3,18 @@
 /** @var yii\web\View $this */
 /** @var string $name */
 /** @var string $message */
+
 /** @var Exception $exception */
 
 use yii\helpers\Html;
 
 $this->title = $name;
 ?>
-<div class="site-error">
+<div class="site-error flex flex-col items-center justify-center h-screen w-full">
 
-    <h1><?= Html::encode($this->title) ?></h1>
+    <h1 class="text-8xl text-main-accent font-sans"><?= Html::encode($this->title) ?></h1>
 
-    <div class="alert alert-danger">
+    <div class="alert alert-danger text-3xl font-sans mb-8">
         <?= nl2br(Html::encode($message)) ?>
     </div>
 
