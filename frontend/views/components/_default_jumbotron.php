3cff116d635e590d3cf3504640e8bcb75055074b
Kamil Pomykała <37307597+Akasiek@users.noreply.github.com>
2022-06-11 20:33:48 +0200

    Update _default_jumbotron.php

--- frontend/views/components/_default_jumbotron.php
+++ frontend/views/components/_default_jumbotron.php
@@ -10,8 +10,8 @@ use common\models\Artist;
 <div class="!bg-cover !bg-center flex flex-col justify-end items-start w-full h-[650px] lg:h-[750px]"
      style="background:
              linear-gradient(180deg, rgba(94, 43, 255, 0) 40.63%,
-             rgba(94, 43, 255, 0.5) 100%,
-             rgba(94, 43, 255, 0.5) 100%),
+             rgba(94, 43, 255, 0.75) 100%,
+             rgba(94, 43, 255, 0.75) 100%),
              url('<?php echo $model->bg_image_url ?>'); ">
     <div class="px-14 pb-10 w-full">
         <h3 class="font-sans text-7xl mb-3"><?php echo $model->name ?></h3>
