a7e33a18de8a44ab4aa5e9b9ce0b9819cb62e730
Kamil Pomykała <37307597+Akasiek@users.noreply.github.com>
2022-06-11 22:30:23 +0200

    Updated the login page

--- frontend/views/site/login.php
+++ frontend/views/site/login.php
@@ -14,32 +14,28 @@ $this->params['breadcrumbs'][] = $this->title;
 <div class="site-login flex flex-col justify-center items-center h-screen w-screen">
     <div class="px-14 py-10 bg-main-dark rounded-3xl">
 
-        <h1 class="font-sans text-3xl"><?= Html::encode($this->title) ?></h1>
+        <h1 class="font-sans text-3xl mb-1"><?= Html::encode($this->title) ?></h1>
 
         <p>Please fill out the following fields to login:</p>
 
-        <div class="flex flex-col gap-6">
-            <?php $form = ActiveForm::begin(['id' => 'login-form']); ?>
+
+        <?php $form = ActiveForm::begin(['id' => 'login-form']); ?>
+        <div class="flex flex-col gap-5 mt-6">
 
             <?= $form->field($model, 'username', [
-                'template' => '
-                                <div class="flex flex-col">
-                                    {label}
-                                    {input}
-                                    <span class="text-red-500">{error}</span>
-                                </div>',
-                'horizontalCssClasses' => [
-                    'wrapper' => 'rounded-3xl bg-secondary-dark border-2 border-main-accent px-4 py-2
-                        focus:outline-0 focus:shadow-accent transition duration-150
-                        placeholder:text-gray-600',
-                ],
-            ])->textInput(['autofocus' => true]) ?>
+                'template' => '<div class="text-lg mb-2">{label}</div>
+                                {input}
+                                <div class="text-red-500 mt-2">{error}</div>',
+            ])->textInput(['autofocus' => true, 'class' => 'input-style']) ?>
 
-            <?= $form->field($model, 'password')->passwordInput() ?>
+            <?= $form->field($model, 'password', ['template' => '
+            <div class="text-lg mb-2">{label}</div>
+            {input}
+            <div class="text-red-500 mt-2">{error}</div>'])->passwordInput(['class' => 'input-style']) ?>
 
             <?= $form->field($model, 'rememberMe')->checkbox() ?>
 
-            <div style="color:#999;margin:1em 0">
+            <div class="text-gray-400">
                 If you forgot your password you can <?= Html::a('reset it', ['site/request-password-reset']) ?>.
                 <br>
                 Need new verification email? <?= Html::a('Resend', ['site/resend-verification-email']) ?>
@@ -47,13 +43,15 @@ $this->params['breadcrumbs'][] = $this->title;
 
             <div class="form-group">
                 <?= Html::submitButton('Login', [
-                    'class' => 'px-4 py-2 bg-dark',
+                    'class' => 'px-5 py-1.5 border-2 border-main-accent rounded-3xl
+                                hover:bg-main-accent hover:text-secondary-dark transition duration-200
+                                font-bold',
                     'name' => 'login-button'
                 ]) ?>
             </div>
 
-            <?php ActiveForm::end(); ?>
         </div>
+        <?php ActiveForm::end(); ?>
 
     </div>
 </div>
