46ead72afeec0140265d075d08f373e29d84f752
Kamil Pomykała <37307597+Akasiek@users.noreply.github.com>
2022-06-12 22:05:31 +0200

    Changes to default jumbotron

--- frontend/views/components/_default_jumbotron.php
+++ frontend/views/components/_default_jumbotron.php
@@ -15,14 +15,14 @@ include Yii::getAlias('@frontend/web/ageDiff.php');
              rgba(94, 43, 255, 0.85) 100%,
              rgba(94, 43, 255, 0.85) 100%),
              url('<?php echo $model->bg_image_url ?>'); ">
-    <div class="px-14 pb-10 w-full">
+    <div class="px-14 pb-10 w-full flex flex-col gap-2">
 
 
-        <div class="flex items-end gap-4">
-            <h3 class="font-sans text-7xl"><?php echo $model->name ?></h3>
+        <div class="flex flex-col gap-1">
             <?php if ($model->full_name): ?>
-                <p class="text-gray-300 italic mb-2"><?php echo $model->full_name ?></p>
+                <p class="text-gray-300 italic"><?php echo $model->full_name ?></p>
             <?php endif; ?>
+            <h3 class="font-sans text-7xl"><?php echo $model->name ?></h3>
         </div>
 
 
@@ -35,7 +35,7 @@ include Yii::getAlias('@frontend/web/ageDiff.php');
 
         <div class="mt-2">
             <?php if ($model->birth_date): ?>
-            
+
                 <p>
                     Age: <?php echo ageDiff($model->birth_date, $model->death_date) ?> years old
                 </p>
