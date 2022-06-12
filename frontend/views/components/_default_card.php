d8553b29981626cdb90fa5bf418b88436d304aa0
Kamil Pomykała <37307597+Akasiek@users.noreply.github.com>
2022-06-12 18:15:58 +0200

    Added cool animations to card component

--- frontend/views/components/_default_card.php
+++ frontend/views/components/_default_card.php
@@ -8,7 +8,8 @@ use yii\helpers\Html;
 
 ?>
 <a href="<?php echo \yii\helpers\Url::to(['/artist/view', 'slug' => $model->slug]) ?>">
-    <div class="rounded-3xl w-full h-48 !bg-cover flex flex-col justify-end items-start snap-start scroll-pl-3"
+    <div class="rounded-3xl w-full h-48 !bg-cover flex flex-col justify-end items-start snap-start scroll-pl-3
+                group overflow-hidden !bg-center"
          style="background: linear-gradient(180deg, rgba(94, 43, 255, 0) 25%,
                  rgba(94, 43, 255, 0.75) 100%),
                  linear-gradient(0deg, rgba(94, 43, 255, 0.1),
@@ -18,13 +19,16 @@ use yii\helpers\Html;
 
         <div class="px-7 pb-4 w-full">
 
-            <h3 class="font-sans text-3xl truncate"><?php echo $model->name ?></h3>
-            <p class="text-sm truncate">
+            <h3 class="font-sans text-3xl truncate xl:translate-y-5
+                       group-hover:translate-y-0 transition-transform">
+                <?php echo $model->name ?>
+            </h3>
+            <p class="text-sm truncate xl:translate-y-10
+                      group-hover:translate-y-0 transition-transform group-hover:delay-100">
                 <?php
                 // TODO: Genres
                 echo "Blues Rock • Experimental Rock"
                 ?>
-
             </p>
         </div>
     </div>
