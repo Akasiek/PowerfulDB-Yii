f2996a06521f0552fbe5c43c0823381824d02910
Kamil Pomykała <37307597+Akasiek@users.noreply.github.com>
2022-06-13 18:59:35 +0200

    Refactoring

---
+++ frontend/views/album/_album_jumbotron.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * @var $model Album
+ * @var $author Artist | Band
+ */
+
+use common\models\Album;
+use common\models\Artist;
+use common\models\Band;
+
+?>
+
+<div class="flex mt-16 justify-center w-full ">
+
+    <div class="flex justify-center items-center gap-10">
+        <div>
+            <img src="<?php echo $model->artwork_url ?>" alt="Album artwork" class="w-96">
+        </div>
+        <div class="flex flex-col gap-2">
+
+            <a href="<?php echo \yii\helpers\Url::to([
+                '/' . ($model->artist_id ? 'artist' : 'band') . '/view/',
+                'slug' => $author->slug
+            ]) ?>" class="hover:underline">
+                <h3 class="text-2xl"><?php echo $author->name ?></h3>
+            </a>
+
+            <h1 class="text-5xl font-bold"><?php echo $model->title ?></h1>
+
+            <p class="text-gray-400"><?php
+                if (isset($model->release_date)) echo Yii::$app->formatter->asDate($model->release_date, 'long');
+                ?></p>
+
+            <p class="italic">
+                <?php
+                // TODO: Genres
+                echo 'Heavy Metal • Hard Rock';
+                ?>
+            </p>
+
+        </div>
+    </div>
+
+</div>
--- frontend/views/album/view.php
+++ frontend/views/album/view.php
@@ -10,33 +10,10 @@ else $author = $model->band;
 
 ?>
 
-<div class="flex mt-16 justify-center w-full ">
 
-    <div class="flex justify-center items-center gap-10">
-        <div>
-            <img src="<?php echo $model->artwork_url ?>" alt="Album artwork" class="w-96">
-        </div>
-        <div class="flex flex-col gap-2">
-
-            <a href="<?php echo \yii\helpers\Url::to([
-                '/' . ($model->artist_id ? 'artist' : 'band') . '/view/',
-                'slug' => $author->slug
-            ]) ?>" class="hover:underline">
-                <h3 class="text-2xl"><?php echo $author->name ?></h3>
-            </a>
-
-            <h1 class="text-5xl font-bold"><?php echo $model->title ?></h1>
-
-            <p class="text-gray-400"><?php echo Yii::$app->formatter->asDate($model->release_date, 'long') ?></p>
-
-            <p class="italic">
-                <?php
-                // TODO: Genres
-                echo 'Heavy Metal • Hard Rock';
-                ?>
-            </p>
-            
-        </div>
-    </div>
-
-</div>
+<?php
+echo $this->render('_album_jumbotron', [
+    'model' => $model,
+    'author' => $author,
+]);
+?>
