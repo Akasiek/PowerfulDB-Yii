cad192a8b41c07849920c3f7b20e15df59aa0c0a
Kamil Pomykała <37307597+Akasiek@users.noreply.github.com>
2022-06-13 14:12:08 +0200

    Refactoring

--- frontend/views/components/_default_card.php
+++ frontend/views/components/_default_card.php
@@ -7,9 +7,10 @@
 use common\models\Artist;
 use common\models\Band;
 use yii\helpers\Html;
+use yii\helpers\Url;
 
 ?>
-<a href="<?php echo \yii\helpers\Url::to(['/' . (isset($baseUrl) ? $baseUrl : Yii::$app->controller->id) . '/view', 'slug' => $model->slug]) ?>">
+<a href="<?php echo Url::to(['/' . (isset($baseUrl) ? $baseUrl : Yii::$app->controller->id) . '/view', 'slug' => $model->slug]) ?>">
     <div class="rounded-3xl w-full h-48 !bg-cover flex flex-col justify-end items-start snap-start scroll-pl-3
                 group overflow-hidden !bg-center"
          style="background: linear-gradient(180deg, rgba(94, 43, 255, 0) 25%,
