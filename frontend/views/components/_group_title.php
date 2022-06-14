a2c9eba9d8ae6aeea40bed37142196c9db9b523f
Kamil Pomykała <37307597+Akasiek@users.noreply.github.com>
2022-06-14 17:56:14 +0200

    Fixed "Add" button hyperlink

--- frontend/views/components/_group_title.php
+++ frontend/views/components/_group_title.php
@@ -9,15 +9,15 @@ use yii\helpers\Url;
         <h1 class="font-sans text-5xl "><?php echo ucfirst(Yii::$app->controller->id) ?> list</h1>
         <?php if (!Yii::$app->user->isGuest): ?>
             <div class="flex">
-                <div class="py-2 pl-5 pr-3 bg-main-accent rounded-3xl text-secondary-dark font-bold">
-                    <a href="<?php echo Url::to('/' . Yii::$app->controller->id . '/create') ?>"
-                       class="flex items-center justify-center gap-2">
+                <a href="<?php echo Url::to('/' . Yii::$app->controller->id . '/create') ?>">
+                    <div class="flex items-center justify-center gap-2
+                    py-2 pl-5 pr-3 bg-main-accent rounded-3xl text-secondary-dark font-bold">
                         Add <?php echo ucfirst(Yii::$app->controller->id) ?>
                         <span class="material-symbols-outlined">
                         add
                     </span>
-                    </a>
-                </div>
+                    </div>
+                </a>
             </div>
         <?php endif; ?>
     </div>
