6b01c418214d676ba64ae782b30ae213cf5ff840
Kamil Pomykała <37307597+Akasiek@users.noreply.github.com>
2022-06-14 17:55:55 +0200

    Update _album_jumbotron.php

--- frontend/views/album/_album_jumbotron.php
+++ frontend/views/album/_album_jumbotron.php
@@ -10,9 +10,9 @@ use common\models\Band;
 
 ?>
 
-<div class="flex mt-16 justify-center w-full ">
+<div class="flex mt-16 justify-center w-full">
 
-    <div class="flex justify-center items-center gap-10">
+    <div class="flex justify-center items-center gap-5 xl:gap-10 max-w-screen-xl px-16">
         <div>
             <img src="<?php echo $model->artwork_url ?>" alt="Album artwork" class="w-96">
         </div>
@@ -22,16 +22,20 @@ use common\models\Band;
                 '/' . ($model->artist_id ? 'artist' : 'band') . '/view/',
                 'slug' => $author->slug
             ]) ?>" class="hover:underline">
-                <h3 class="text-2xl"><?php echo $author->name ?></h3>
+                <h3 class="text-lg lg:text-2xl"><?php echo $author->name ?></h3>
             </a>
 
-            <h1 class="text-5xl font-bold"><?php echo $model->title ?></h1>
+            <h1 class="text-2xl lg:text-5xl font-bold"><?php echo $model->title ?></h1>
 
-            <p class="text-gray-400"><?php
-                if (isset($model->release_date)) echo Yii::$app->formatter->asDate($model->release_date, 'long');
-                ?></p>
+            <p class="text-gray-400 text-sm lg:text-base">
+                <?php
+                if (isset($model->release_date)) {
+                    echo Yii::$app->formatter->asDate($model->release_date, 'long');
+                }
+                ?>
+            </p>
 
-            <p class="italic">
+            <p class="italic text-sm lg:text-base">
                 <?php
                 // TODO: Genres
                 echo 'Heavy Metal • Hard Rock';
