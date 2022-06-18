cf85500a43c8a2e956614e1d5d33ae66e29f26ea
Kamil Pomykała <37307597+Akasiek@users.noreply.github.com>
2022-06-18 17:59:49 +0200

    Artist Article admin panel update

--- backend/views/artist-article/_form.php
+++ backend/views/artist-article/_form.php
@@ -1,20 +1,37 @@
 <?php
 
+use common\models\Artist;
+use dosamigos\tinymce\TinyMce;
+use yii\helpers\ArrayHelper;
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
 
 /* @var $this yii\web\View */
 /* @var $model common\models\ArtistArticle */
 /* @var $form yii\widgets\ActiveForm */
+
+$artistModels = Artist::find()->asArray()->all();
+$artistMap = ArrayHelper::map($artistModels, 'id', 'name');
 ?>
 
 <div class="artist-article-form">
 
     <?php $form = ActiveForm::begin(); ?>
 
-    <?= $form->field($model, 'artist_id')->textInput() ?>
+    <?= $form->field($model, 'artist_id')->dropDownList($artistMap, [
+        'prompt' => 'Select an artist',
+    ]) ?>
 
-    <?= $form->field($model, 'text')->textarea(['rows' => 6]) ?>
+    <?= $form->field($model, 'text')->widget(TinyMce::className(), [
+        'options' => ['rows' => 6],
+        'language' => 'en_GB',
+        'clientOptions' => [
+            'plugins' => [
+                'image', 'emoticons', 'autolink'
+            ],
+            'toolbar' => 'undo redo | styleselect blocks | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image emoticons',
+        ]
+    ]) ?>
 
     <div class="form-group">
         <?= Html::submitButton('Save', ['class' => 'btn btn-success']) ?>
