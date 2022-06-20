7634eaabde91073f44dc8182e227d4f8ca26760e
Kamil Pomykała <37307597+Akasiek@users.noreply.github.com>
2022-06-20 18:57:19 +0200

    Fixed AJAX issue

--- frontend/views/components/_author_albums.php
+++ frontend/views/components/_author_albums.php
@@ -28,101 +28,101 @@ if (Yii::$app->request->isPjax && Yii::$app->request->post('displayStyle')) {
 
 ?>
 
+<?php Pjax::begin(['id' => 'album-display-style-pjax']); ?>
+    <div class="w-full m-auto">
+        <div class="flex gap-20 justify-between items-center">
 
-<div class="w-full m-auto">
-    <div class="flex gap-20 justify-between items-center">
-
-        <div class="flex items-center gap-4">
-            <h1 class="font-sans text-5xl">Albums</h1>
-            <?= Html::a('add', ['/album/create', Yii::$app->controller->id . '_id' => $model->id],
-                ['class' => 'material-symbols-rounded text-secondary-dark p-0.5 rounded-full bg-main-accent']) ?>
-        </div>
-
-        <div class="flex gap-4">
-
-            <?= Html::button('grid_view', [
-                'class' => 'material-symbols-rounded !text-3xl',
-                'data-method' => 'post',
-                'data-pjax' => '1',
-                'data-params' => [
-                    'displayStyle' => 'grid',
-                ],
-            ]) ?>
-            <?= Html::button('list', [
-                'class' => 'material-symbols-rounded !text-3xl',
-                'data-method' => 'post',
-                'data-pjax' => '1',
-                'data-params' => [
-                    'displayStyle' => 'list',
-                ],
-            ]) ?>
-
-        </div>
-    </div>
-    <hr class="max-w-sm   border-t-2 border-t-main-accent mt-2 mb-6">
-    <?php if ($albumsCount !== 0): ?>
-        <?php Pjax::begin(['id' => 'album-display-style-pjax']); ?>
-        <?php if ($displayStyle === 'grid'): ?>
-            <div class="grid grid-cols-2 lg:grid-cols-4 gap-x-8 gap-y-12">
-                <?php foreach ($albums as $album): ?>
-                    <a href="<?= Url::to(['/album/view', 'slug' => $album->slug,]) ?>"
-                       class="group transition">
-                        <div class="text-center">
-                            <img class="mb-2" src="<?= $album->artwork_url ?>" alt="Album artwork">
-
-                            <p class="text-base lg:text-lg truncate group-hover:underline"
-                               title="<?= $album->title ?>">
-                                <?= $album->title ?>
-                            </p>
-
-                            <p class="text-sm truncate text-gray-400">
-                                <?= Yii::$app->formatter->asDate($album->release_date, 'Y') ?>
-                            </p>
-                        </div>
-                    </a>
-                <?php endforeach ?>
+            <div class="flex items-center gap-4">
+                <h1 class="font-sans text-5xl">Albums</h1>
+                <?= Html::a('add', ['/album/create', Yii::$app->controller->id . '_id' => $model->id],
+                    ['class' => 'material-symbols-rounded text-secondary-dark p-0.5 rounded-full bg-main-accent']) ?>
             </div>
 
-        <?php elseif ($displayStyle === 'list'): ?>
+            <div class="flex gap-4">
 
-            <div class="flex flex-col justify-center items-center m-auto">
-                <?php $i = 0 ?>
-                <?php foreach ($albums as $album): ?>
-                    <a href="<?= Url::to(['/album/view', 'slug' => $album->slug,]) ?>"
-                       class="w-full">
-                        <div class="flex justify-center items-center w-full">
-                            <div class="h-36 m-10">
-                                <img src="<?= $album->artwork_url ?>" alt="Album artwork" class="h-full">
+                <?= Html::button('grid_view', [
+                    'class' => 'material-symbols-rounded !text-3xl',
+                    'data-method' => 'post',
+                    'data-pjax' => '1',
+                    'data-params' => [
+                        'displayStyle' => 'grid',
+                    ],
+                ]) ?>
+                <?= Html::button('list', [
+                    'class' => 'material-symbols-rounded !text-3xl',
+                    'data-method' => 'post',
+                    'data-pjax' => '1',
+                    'data-params' => [
+                        'displayStyle' => 'list',
+                    ],
+                ]) ?>
 
-                            </div>
+            </div>
+        </div>
+        <hr class="max-w-sm   border-t-2 border-t-main-accent mt-2 mb-6">
+        <?php if ($albumsCount !== 0): ?>
+            <?php if ($displayStyle === 'grid'): ?>
+                <div class="grid grid-cols-2 lg:grid-cols-4 gap-x-8 gap-y-12">
+                    <?php foreach ($albums as $album): ?>
+                        <a href="<?= Url::to(['/album/view', 'slug' => $album->slug,]) ?>"
+                           class="group transition">
+                            <div class="text-center">
+                                <img class="mb-2" src="<?= $album->artwork_url ?>" alt="Album artwork">
 
-                            <div class="col-span-2 w-full flex-1">
-                                <h2 class="text-2xl"><?= $album->title ?></h2>
-                                <p class="italic text-gray-400">
-                                    <?= Yii::$app->formatter->asDate($album->release_date, 'long') ?>
+                                <p class="text-base lg:text-lg truncate group-hover:underline"
+                                   title="<?= $album->title ?>">
+                                    <?= $album->title ?>
+                                </p>
+
+                                <p class="text-sm truncate text-gray-400">
+                                    <?= Yii::$app->formatter->asDate($album->release_date, 'Y') ?>
                                 </p>
                             </div>
-                        </div>
-                    </a>
-                    <?php if (++$i !== $albumsCount): ?>
-                        <hr class="my-8 border-t-2 border-t-gray-700 w-[60%] mx-auto">
-                    <?php endif ?>
+                        </a>
+                    <?php endforeach ?>
+                </div>
 
-                <?php endforeach ?>
+            <?php elseif ($displayStyle === 'list'): ?>
+
+                <div class="flex flex-col justify-center items-center m-auto">
+                    <?php $i = 0 ?>
+                    <?php foreach ($albums as $album): ?>
+                        <a href="<?= Url::to(['/album/view', 'slug' => $album->slug,]) ?>"
+                           class="w-full">
+                            <div class="flex justify-center items-center w-full">
+                                <div class="h-36 m-10">
+                                    <img src="<?= $album->artwork_url ?>" alt="Album artwork" class="h-full">
+
+                                </div>
+
+                                <div class="col-span-2 w-full flex-1">
+                                    <h2 class="text-2xl"><?= $album->title ?></h2>
+                                    <p class="italic text-gray-400">
+                                        <?= Yii::$app->formatter->asDate($album->release_date, 'long') ?>
+                                    </p>
+                                </div>
+                            </div>
+                        </a>
+                        <?php if (++$i !== $albumsCount): ?>
+                            <hr class="my-8 border-t-2 border-t-gray-700 w-[60%] mx-auto">
+                        <?php endif ?>
+
+                    <?php endforeach ?>
+                </div>
+
+            <?php endif ?>
+
+
+        <?php else: ?>
+
+            <div class="article-style text-justify">
+                <p>This <?= Yii::$app->controller->id ?> has no albums yet. You can go ahead and
+                    <?= Html::a('add album by this ' . ($baseUrl ?? Yii::$app->controller->id),
+                        ['/album/create', Yii::$app->controller->id . '_id' => $model->id],
+                        ['class' => 'underline hover:text-main-accent transition-colors']) ?>
+                </p>
             </div>
 
-        <?php endif ?>
-        <?php Pjax::end(); ?>
-
-    <?php else: ?>
-
-        <div class="article-style text-justify">
-            <p>This <?= Yii::$app->controller->id ?> has no albums yet. You can go ahead and
-                <?= Html::a('add album by this ' . ($baseUrl ?? Yii::$app->controller->id),
-                    ['/album/create', Yii::$app->controller->id . '_id' => $model->id],
-                    ['class' => 'underline hover:text-main-accent transition-colors']) ?>
-            </p>
-        </div>
-
-    <?php endif; ?>
-</div>
+        <?php endif; ?>
+    </div>
+<?php Pjax::end(); ?>
