1814653697173cb3d1dd4ec85570ae6f5decec87
Kamil Pomykała <37307597+Akasiek@users.noreply.github.com>
2022-06-23 00:45:18 +0200

    Update _index_page_title.php

--- frontend/views/components/_index_page_title.php
+++ frontend/views/components/_index_page_title.php
@@ -10,10 +10,9 @@ use yii\helpers\Url;
         <?php if (!Yii::$app->user->isGuest): ?>
             <div class="flex">
                 <a href="<?php echo Url::to('/' . Yii::$app->controller->id . '/create') ?>">
-                    <div class="flex items-center justify-center gap-2
-                    py-2 pl-5 pr-3 bg-main-accent rounded-3xl text-secondary-dark font-bold">
+                    <div class="btn-style flex justify-center items-center gap-1 py-2 pl-5 pr-3">
                         Add <?php echo ucfirst(Yii::$app->controller->id) ?>
-                        <span class="material-symbols-outlined">
+                        <span class="material-symbols-rounded">
                         add
                     </span>
                     </div>
