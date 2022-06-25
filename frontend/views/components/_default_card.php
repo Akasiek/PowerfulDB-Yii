46f7de4adbfef25fd05282d72454229250d57abf
Kamil Pomykała <37307597+Akasiek@users.noreply.github.com>
2022-06-25 02:50:06 +0200

    Artist and Band Cards CSS Change

--- frontend/views/components/_default_card.php
+++ frontend/views/components/_default_card.php
@@ -12,21 +12,26 @@ use yii\helpers\Url;
 ?>
 <a href="<?php echo Url::to(['/' . (isset($baseUrl) ? $baseUrl : Yii::$app->controller->id) . '/view', 'slug' => $model->slug]) ?>">
     <div class="rounded-3xl w-full h-48 !bg-cover flex flex-col justify-end items-start snap-start scroll-pl-3
-                group overflow-hidden !bg-center"
-         style="background: linear-gradient(180deg, rgba(94, 43, 255, 0) 25%,
-                 rgba(94, 43, 255, 0.75) 100%),
-                 linear-gradient(0deg, rgba(94, 43, 255, 0.1),
-                 rgba(94, 43, 255, 0.1)),
-                 url('<?php echo $model->bg_image_url ?>'),
-                 #292A33">
+                group overflow-hidden !bg-center relative group bg-secondary-dark"
+    >
 
-        <div class="px-7 pb-4 w-full">
+        <img src="<?= $model->bg_image_url ?>" alt=""
+             class="absolute h-full w-full object-center object-cover z-10
+                    transition-all xl:group-hover:scale-110 opacity-95 xl:opacity-80 group-hover:opacity-100"
+        >
+
+        <div class="absolute top-0 bottom-0 left-0 right-0 w-full z-20 group-hover:scale-125 transition-all"
+             style="background: linear-gradient(180deg, rgba(94, 43, 255, 0),
+            rgba(94, 43, 255, 0.5))">
+        </div>
+
+        <div class="px-7 pb-4 w-full z-30">
 
             <h3 class="font-sans text-3xl truncate xl:translate-y-5
-                       group-hover:translate-y-0 transition-transform">
+                       group-hover:translate-y-0 transition-transform drop-shadow-[0_2px_4px_rgba(0,0,0,0.25)]">
                 <?php echo $model->name ?>
             </h3>
-            <p class="text-sm truncate xl:translate-y-10
+            <p class="text-sm truncate xl:translate-y-10 drop-shadow-md
                       group-hover:translate-y-0 transition-transform group-hover:delay-100">
                 <?php
                 // TODO: Genres
