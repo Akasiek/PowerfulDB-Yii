92b86fafa13914ce2c8a9546b6e84f8de6531e4a
Kamil Pomykała <37307597+Akasiek@users.noreply.github.com>
2022-06-26 00:19:28 +0200

    Fixed not working submit button

--- frontend/views/layouts/sidebar/sidebar.php
+++ frontend/views/layouts/sidebar/sidebar.php
@@ -7,37 +7,30 @@ use yii\widgets\Pjax;
 ?>
 
 
-<aside class="h-screen !w-72 w-96 position-fixed transition-all" id="sidebar">
+<aside class="h-screen w-72 position-fixed transition-all" id="sidebar">
     <div class="bg-main-dark h-full p-5 gap-6 flex flex-col font-serif font-bold relative">
 
         <!-- LOGO -->
         <div class="flex items-center justify-center">
-            <a href="<?php echo Url::to('/') ?>" class="h-11">
-                <?php echo Html::img('@web/resources/logo/logo.svg',
+            <a href="<?= Url::to('/') ?>" class="h-11">
+                <?= Html::img(
+                    '@web/resources/logo/logo.svg',
                     [
                         'class' => 'h-full',
-                        'alt' => 'App logo'
-                    ]) ?>
+                        'alt' => 'App logo',
+                    ]
+                ) ?>
             </a>
         </div>
 
 
         <!-- SEARCHBAR -->
         <form action="/search" method="get" class="relative flex items-center">
-            <input
-                    class="w-full px-4 py-1.5 border-2 border-main-accent rounded-3xl bg-transparent font-bold text-lg text-main-light
+            <input class="w-full px-4 py-1.5 border-2 border-main-accent rounded-3xl bg-transparent font-bold text-lg text-main-light
                     focus:outline-none focus:bg-main-accent focus:text-secondary-dark focus:placeholder:text-secondary-dark
-                    shadow-accent peer transition-all duration-300"
-                    id="search-input"
-                    type="text"
-                    placeholder="Search..."
-                    autocomplete="none"
-                    name="keyword"
-                    minlength="2">
+                    shadow-accent peer transition-all duration-300" id="search-input" type="text" placeholder="Search..." autocomplete="none" name="keyword" minlength="2">
             <div class="absolute flex right-4 text-main-accent peer-focus:text-main-dark transition-all duration-300">
-                    <span class="material-symbols-rounded cursor-pointer ">
-                        search
-                    </span>
+                <input type="submit" value="search" class="material-symbols-rounded cursor-pointer" />
             </div>
 
         </form>
@@ -45,31 +38,31 @@ use yii\widgets\Pjax;
         <!-- MAIN OPTIONS -->
         <div class="flex flex-col gap-3">
 
-            <?php echo $this->render('_main_option', [
+            <?= $this->render('_main_option', [
                 'text' => 'Home Page',
                 'icon' => 'home',
                 'url' => '/',
             ]) ?>
 
-            <?php echo $this->render('_main_option', [
+            <?= $this->render('_main_option', [
                 'text' => 'Artists',
                 'icon' => 'mic_external_on',
                 'url' => '/artist',
             ]) ?>
 
-            <?php echo $this->render('_main_option', [
+            <?= $this->render('_main_option', [
                 'text' => 'Bands',
                 'icon' => 'groups',
                 'url' => '/band',
             ]) ?>
 
-            <?php echo $this->render('_main_option', [
+            <?= $this->render('_main_option', [
                 'text' => 'Albums',
                 'icon' => 'album',
                 'url' => '/album',
             ]) ?>
 
-            <?php echo $this->render('_main_option', [
+            <?= $this->render('_main_option', [
                 'text' => 'Users',
                 'icon' => 'account_circle',
                 'url' => '/user',
@@ -79,7 +72,7 @@ use yii\widgets\Pjax;
         <hr class="border-t-2 border-t-[rgba(255,255,255,0.15)]">
 
         <!-- SIDE OPTIONS -->
-        <?php echo $this->render('_main_option', [
+        <?= $this->render('_main_option', [
             'text' => 'About',
             'icon' => 'info',
             'url' => '/about',
@@ -91,9 +84,8 @@ use yii\widgets\Pjax;
     <!-- PROFILE / LOG IN -->
     <div class="text-secondary-dark bg-main-accent absolute left-0 right-0 bottom-0 px-4 py-3">
         <?php
-        if (Yii::$app->user->isGuest): ?>
-            <a href="<?php echo Url::to('/site/login') ?>"
-               class="flex items-center justify-start gap-4">
+        if (Yii::$app->user->isGuest) : ?>
+            <a href="<?= Url::to('/site/login') ?>" class="flex items-center justify-start gap-4">
                 <p class="material-symbols-outlined ">
                     login
                 </p>
@@ -101,20 +93,22 @@ use yii\widgets\Pjax;
                     Log in
                 </p>
             </a>
-        <?php else: ?>
+        <?php else : ?>
             <div class="flex items-center gap-4">
                 <div class="aspect-square flex-none">
                     <!-- TODO: Avatar display-->
-                    <?php echo Html::a(
+                    <?= Html::a(
                         Html::img(
                             'https://icon-library.com/images/default-profile-icon/default-profile-icon-16.jpg',
                             ['class' => 'rounded-full h-8 object-cover']
-                        ), ['/users/view', 'id' => Yii::$app->user->identity->id])
+                        ),
+                        ['/users/view', 'id' => Yii::$app->user->identity->id]
+                    )
                     ?>
                 </div>
                 <div class="truncate pr-6">
                     <p class="text-xl font-bold truncate">
-                        <?php echo Html::a(
+                        <?= Html::a(
                             Yii::$app->user->identity->username,
                             ['/users/view', 'id' => Yii::$app->user->identity->id]
                         ) ?>
@@ -122,9 +116,7 @@ use yii\widgets\Pjax;
                 </div>
 
                 <div class="absolute right-4 flex">
-                    <a href="<?php echo Url::to('/site/logout') ?>"
-                       data-method="post"
-                       class="material-symbols-outlined text-secondary-dark font-normal">
+                    <a href="<?= Url::to('/site/logout') ?>" data-method="post" class="material-symbols-outlined text-secondary-dark font-normal">
                         logout
                     </a>
                 </div>
@@ -133,14 +125,4 @@ use yii\widgets\Pjax;
 
     </div>
 
-
 </aside>
-
-
-<script>
-    const sidebar = document.getElementById('sidebar');
-    const searchInput = document.getElementById('search-input');
-
-    searchInput.addEventListener('focus', () => sidebar.classList.remove('!w-72'));
-    searchInput.addEventListener('blur', () => sidebar.classList.add('!w-72'));
-</script>
