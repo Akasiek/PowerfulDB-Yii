220ac6b51ebaccd1e48554608165a16934f6b31b
Kamil Pomykała <37307597+Akasiek@users.noreply.github.com>
2022-07-09 00:49:20 +0200

    Update _genre_view.php

--- frontend/views/genre/_genre_view.php
+++ frontend/views/genre/_genre_view.php
@@ -16,15 +16,15 @@ use yii\helpers\Url;
             <h1 class="text-3xl font-bold "><?= $model->name ?></h1>
         </a>
 
-        <a href="<?= Url::to('/album/?genre=' . $model->name) ?>" class="text-lg text-gray-400 ">
+        <a href="<?= Url::to('/album/?genre=' . $model->name) ?>" class="text-lg text-gray-400 transition-colors <?php if ($model->countAlbum !== 0) echo 'hover:text-main-accent' ?>">
             <?= $model->countAlbum ?> albums
         </a>
 
-        <a href="<?= Url::to('/band/?genre=' . $model->name) ?>" class="text-lg text-gray-400 ">
+        <a href="<?= Url::to('/band/?genre=' . $model->name) ?>" class="text-lg text-gray-400 transition-colors <?php if ($model->countBand !== 0) echo 'hover:text-main-accent' ?>">
             <?= $model->countBand ?> bands
         </a>
 
-        <a href="<?= Url::to('/artist/?genre=' . $model->name) ?>" class="text-lg text-gray-400 ">
+        <a href="<?= Url::to('/artist/?genre=' . $model->name) ?>" class="text-lg text-gray-400 transition-colors <?php if ($model->countArtist !== 0) echo 'hover:text-main-accent' ?> ">
             <?= $model->countArtist ?> artists
         </a>
 
