a54944549629d828377713045c2a85dfb6017c37
Kamil Pomykała <37307597+Akasiek@users.noreply.github.com>
2022-07-09 22:37:46 +0200

    Mobile design for User Card component

--- frontend/views/user/_user_card.php
+++ frontend/views/user/_user_card.php
@@ -10,12 +10,12 @@ use yii\helpers\Url;
 ?>
 <a href="<?= Url::to(['/user/view', 'id' => $model->id]) ?>">
     <div class="bg-main-dark rounded-2xl overflow-hidden">
-        <div class="bg-main-accent py-4 px-2 flex items-center justify-center">
-            <img src="<?= $model->profile_pic_url ?? 'https://icon-library.com/images/default-profile-icon/default-profile-icon-16.jpg' ?>" alt="" class="rounded-full object-cover object-center aspect-square h-16 shadow-lg">
+        <div class="bg-main-accent py-2 md:py-4 px-2 flex items-center justify-center">
+            <img src="<?= $model->profile_pic_url ?? 'https://icon-library.com/images/default-profile-icon/default-profile-icon-16.jpg' ?>" alt="" class="rounded-full object-cover object-center aspect-square h-12 md:h-14 lg:h-16 shadow-lg">
         </div>
 
-        <div class="py-5 flex items-center px-4 justify-center truncate">
-            <h3 class="font-bold text-xl truncate" title="<?= $model->username ?>">
+        <div class="flex items-center justify-center truncate px-2 md:px-4 py-3 md:py-5">
+            <h3 class="font-bold text-base md:text-xl truncate" title="<?= $model->username ?>">
                 <?= $model->username ?>
             </h3>
         </div>
