a6715ba5dd262cddf65512e944c7e85f199a1df9
Kamil Pomykała <37307597+Akasiek@users.noreply.github.com>
2022-07-09 21:55:05 +0200

    Mobile design changes in default card

--- frontend/views/components/_default_card.php
+++ frontend/views/components/_default_card.php
@@ -14,7 +14,7 @@ $genres = $model->getGenres()->limit(4)->all();
 
 ?>
 <a href="<?php echo Url::to(['/' . (isset($baseUrl) ? $baseUrl : Yii::$app->controller->id) . '/view', 'slug' => $model->slug]) ?>">
-    <div class="rounded-3xl w-full h-48 !bg-cover flex flex-col justify-end items-start snap-start scroll-pl-3
+    <div class="rounded-3xl w-full h-40 md:h-48 !bg-cover flex flex-col justify-end items-start snap-start scroll-pl-3
                 group overflow-hidden !bg-center relative group bg-secondary-dark">
 
         <img src="<?= $model->bg_image_url ?>" alt="" class="absolute h-full w-full object-center object-cover z-10
@@ -24,15 +24,15 @@ $genres = $model->getGenres()->limit(4)->all();
             rgba(94, 43, 255, 0.5))">
         </div>
 
-        <div class="px-7 pb-4 w-full z-30">
+        <div class="px-4 md:px-7 pb-3 md:pb-4 w-full z-30">
 
-            <h3 class="font-bold text-2xl truncate 
+            <h3 class="font-bold text-xl md:text-2xl truncate 
             transition-transform drop-shadow-[0_2px_4px_rgba(0,0,0,0.15)]">
                 <?php echo $model->name ?>
             </h3>
 
             <?php if (!empty($genres)) : ?>
-                <p class="text-sm truncate  drop-shadow-md
+                <p class="text-xs md:text-sm truncate  drop-shadow-md
                       transition-transform group-hover:delay-100">
                     <?php
                     foreach ($genres as $index => $genre) {
