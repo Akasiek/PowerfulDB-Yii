cf780f4a0e12137f672093451b6ab3937162f0a3
Kamil Pomykała <37307597+Akasiek@users.noreply.github.com>
2022-07-11 16:45:06 +0200

    Made menu options more accessible on mobile

--- frontend/views/layouts/sidebar/_main_option.php
+++ frontend/views/layouts/sidebar/_main_option.php
@@ -11,16 +11,15 @@ use yii\helpers\Url;
 ?>
 <!-- md:bg-transparent md:p-0 md:rounded-none bg-secondary-dark py-1 px-4 rounded-2xl -->
 
-<div class="main-options flex">
-    <div class="option text-main-light hover:text-main-accent transition-colors cursor-pointer hover:shadow-accent
-    ">
-        <a class="flex items-center justify-start gap-4" href="<?php echo Url::to([$url]) ?>">
-            <span class="material-symbols-rounded !text-2xl lg:!text-3xl">
-                <?= $icon ?>
-            </span>
-            <div class="text-base lg:text-lg">
-                <?= $text ?>
-            </div>
-        </a>
-    </div>
+<div class="main-options flex flex-1">
+    <a class="w-full md:w-fit flex items-center justify-start gap-4
+    option text-main-light hover:text-main-accent transition-colors
+    cursor-pointer hover:shadow-accent" href="<?php echo Url::to([$url]) ?>">
+        <span class="material-symbols-rounded !text-2xl lg:!text-3xl">
+            <?= $icon ?>
+        </span>
+        <div class="text-base lg:text-lg">
+            <?= $text ?>
+        </div>
+    </a>
 </div>
