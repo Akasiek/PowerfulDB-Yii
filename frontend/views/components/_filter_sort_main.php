606b7b892b4dc4a35420c1d1b2f401a39c62048f
Kamil Pomykała <37307597+Akasiek@users.noreply.github.com>
2022-07-13 16:44:29 +0200

    Fixed Filter Sort component - not close div 🤦‍♂️

--- frontend/views/components/_filter_sort_main.php
+++ frontend/views/components/_filter_sort_main.php
@@ -12,95 +12,93 @@
 use yii\data\Sort;
 use yii\helpers\Html;
 ?>
-<div>
-    <?= Html::beginForm(['/' . Yii::$app->controller->id . '/'], 'get'); ?>
 
-    <div class="flex flex-col md:flex-row md:flex-wrap items-center justify-between gap-5 mb-10 mt-[-1rem]">
+<?= Html::beginForm(['/' . Yii::$app->controller->id . '/'], 'get'); ?>
+<div class="flex flex-col md:flex-row md:flex-wrap items-center justify-between gap-5 mb-10 mt-[-1rem]">
 
-        <div class="flex flex-col md:flex-row flex-wrap items-center gap-x-8 gap-y-4">
+    <div class="flex flex-col md:flex-row flex-wrap items-center gap-x-8 gap-y-4 flex-1">
+        <?php if (isset($sort)) : ?>
+            <div>
+                <?php
+                $sortValue = key($sort->getAttributeOrders());
+                if (array_values($sort->getAttributeOrders())[0] == 3) {
+                    $sortValue = '-' . $sortValue;
+                }
 
+                echo Html::dropDownList('sort', $sortValue, $sortOptions, [
+                    'class' => 'input-style m-0 !py-2',
+                    'id' => 'select-slim',
+                    'onchange' => 'this.form.submit()',
+                ]) ?>
+            </div>
+        <?php endif; ?>
 
-            <?php if (isset($sort)) : ?>
-                <div>
-                    <?php
-                    $sortValue = key($sort->getAttributeOrders());
-                    if (array_values($sort->getAttributeOrders())[0] == 3) {
-                        $sortValue = '-' . $sortValue;
-                    }
+        <div class="flex flex-col items-center gap-3">
+            <p class="md:hidden flex items-center gap-2 btn-style" id="filters-text">
+                Filters menu
+                <span class="material-symbols-outlined !text-xl transition-transform" id="expand-icon">
+                    expand_more
+                </span>
+            </p>
+            <div class="hidden md:flex flex-col md:flex-row gap-4 flex-wrap justify-start items-start" id="filters-form">
 
-                    echo Html::dropDownList('sort', $sortValue, $sortOptions, [
-                        'class' => 'input-style m-0 !py-2',
-                        'id' => 'select-slim',
-                        'onchange' => 'this.form.submit()',
-                    ]) ?>
-                </div>
-            <?php endif; ?>
+                <?php if (isset($yearFilters)) foreach ($yearFilters as $name => $filter) : ?>
+                    <div class="filter-container">
+                        <p><?= $filter['label'] ?></p>
+                        <div class="filter-input-container">
 
-            <div class="flex flex-col items-center gap-3">
-                <p class="md:hidden flex items-center gap-2 btn-style" id="filters-text">
-                    Filters menu
-                    <span class="material-symbols-outlined !text-xl transition-transform" id="expand-icon">
-                        expand_more
-                    </span>
-                </p>
-                <div class="hidden md:flex flex-col md:flex-row gap-4 flex-wrap justify-start items-start" id="filters-form">
+                            <input type="number" name="<?= $name ?>_from_year" value="<?= $filter['from_year'] ?>" class="w-12 text-center filter-input-style" min="1900" max="2099" placeholder="1900">
 
-                    <?php if (isset($yearFilters)) foreach ($yearFilters as $name => $filter) : ?>
-                        <div class="filter-container">
-                            <p><?= $filter['label'] ?></p>
-                            <div class="filter-input-container">
+                            <p>-</p>
 
-                                <input type="number" name="<?= $name ?>_from_year" value="<?= $filter['from_year'] ?>" class="w-12 text-center filter-input-style" min="1900" max="2099" placeholder="1900">
-
-                                <p>-</p>
-
-                                <input type="number" name="<?= $name ?>_to_year" value="<?= $filter['to_year'] ?>" class="w-12 text-center filter-input-style" min="1900" max="2099" placeholder="<?= date('Y') ?>">
-                            </div>
+                            <input type="number" name="<?= $name ?>_to_year" value="<?= $filter['to_year'] ?>" class="w-12 text-center filter-input-style" min="1900" max="2099" placeholder="<?= date('Y') ?>">
                         </div>
-                    <?php endforeach; ?>
+                    </div>
+                <?php endforeach; ?>
 
-                    <?php if (isset($stringFilters)) foreach ($stringFilters as $name => $filter) : ?>
-                        <div class="filter-container">
-                            <p><?= $filter['label'] ?></p>
-                            <div class="filter-input-container">
+                <?php if (isset($stringFilters)) foreach ($stringFilters as $name => $filter) : ?>
+                    <div class="filter-container">
+                        <p><?= $filter['label'] ?></p>
+                        <div class="filter-input-container">
 
-                                <input type="text" name="<?= $name ?>" value="<?= $filter['value'] ?>" class="w-36 filter-input-style" placeholder="<?= $filter['placeholder'] ?>">
-                            </div>
-                        <?php endforeach; ?>
+                            <input type="text" name="<?= $name ?>" value="<?= $filter['value'] ?>" class="w-36 filter-input-style" placeholder="<?= $filter['placeholder'] ?>">
+                        </div>
+                    </div>
+                <?php endforeach; ?>
 
-                        <?php if (isset($hasGenreFilter) && $hasGenreFilter) : ?>
-                            <div class="filter-container">
-                                <p>Genre:</p>
-                                <div class="filter-input-container">
+                <?php if (isset($hasGenreFilter) && $hasGenreFilter) : ?>
+                    <div class="filter-container">
+                        <p>Genre:</p>
+                        <div class="filter-input-container">
 
-                                    <input type="text" name="genre" value="<?= $genre ?>" class="w-28 filter-input-style" placeholder="Rock">
-
-                                </div>
-                            </div>
-                        <?php endif; ?>
+                            <input type="text" name="genre" value="<?= $genre ?>" class="w-28 filter-input-style" placeholder="Rock">
 
                         </div>
-                </div>
-            </div>
+                    </div>
+                <?php endif; ?>
 
-            <div class="flex gap-5 ml-auto">
-                <input type="submit" value="Filter" class="btn-style">
-                <?= Html::a('Reset', ['/' . Yii::$app->controller->id], ['class' => 'btn-style']) ?>
             </div>
-
         </div>
-        <?= Html::endForm() ?>
+
+
     </div>
 
-    <script>
-        // On filters text click, toggle the filters form
-        const filtersText = document.getElementById('filters-text');
-        const filtersForm = document.getElementById('filters-form');
-        const expandIcon = document.getElementById('expand-icon');
+    <div class="flex gap-5 ml-auto">
+        <input type="submit" value="Filter" class="btn-style">
+        <?= Html::a('Reset', ['/' . Yii::$app->controller->id], ['class' => 'btn-style']) ?>
+    </div>
+</div>
+<?= Html::endForm() ?>
 
-        filtersText.addEventListener('click', () => {
-            filtersForm.classList.toggle('hidden');
-            filtersForm.classList.toggle('flex');
-            expandIcon.classList.toggle('!-rotate-180');
-        });
-    </script>
+<script>
+    // On filters text click, toggle the filters form
+    const filtersText = document.getElementById('filters-text');
+    const filtersForm = document.getElementById('filters-form');
+    const expandIcon = document.getElementById('expand-icon');
+
+    filtersText.addEventListener('click', () => {
+        filtersForm.classList.toggle('hidden');
+        filtersForm.classList.toggle('flex');
+        expandIcon.classList.toggle('!-rotate-180');
+    });
+</script>
