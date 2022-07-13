c0db636f82512b648b1afcbc72019c4d44bf20ea
Kamil Pomykała <37307597+Akasiek@users.noreply.github.com>
2022-07-13 17:45:01 +0200

    Added custom favicon

--- frontend/views/layouts/base.php
+++ frontend/views/layouts/base.php
@@ -21,6 +21,7 @@ AppAsset::register($this);
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <?php $this->registerCsrfMetaTags() ?>
     <title><?= Html::encode($this->title . " | PowerfulDB") ?></title>
+    <link rel="icon" href="<?= Yii::getAlias('@web/resources/logo/favicon.svg') ?>" type="image/svg+xml">
     <?php $this->head() ?>
 
     <!-- GOOGLE FONTS -->
