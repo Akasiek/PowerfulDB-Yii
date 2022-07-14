29c5be1d9dadfa092a4d79f3ccf04a63e231ff8a
Kamil Pomykała <37307597+Akasiek@users.noreply.github.com>
2022-07-14 16:22:12 +0200

    Responsive design for Error page

--- frontend/views/site/error.php
+++ frontend/views/site/error.php
@@ -10,11 +10,12 @@ use yii\helpers\Html;
 
 $this->title = $name;
 ?>
-<div class="site-error flex flex-col items-center justify-center h-screen w-full">
+<div class="h-screen w-full flex flex-col items-center justify-center px-6
+            text-center text-xs sm:text-sm md:text-base">
 
-    <h1 class="text-8xl text-main-accent font-sans"><?= Html::encode($this->title) ?></h1>
+    <h1 class="text-5xl sm:text-6xl md:text-7xl lg:text-8xl text-main-accent font-sans"><?= Html::encode($this->title) ?></h1>
 
-    <div class="alert alert-danger text-3xl font-sans mb-8">
+    <div class="alert alert-danger text-2xl md:text-3xl font-sans mb-4 md:mb-8">
         <?= nl2br(Html::encode($message)) ?>
     </div>
 
@@ -24,5 +25,8 @@ $this->title = $name;
     <p>
         Please contact us if you think this is a server error. Thank you.
     </p>
+    <a href="https://youtu.be/dQw4w9WgXcQ" class="text-gray-700 font-bold mt-1">
+        ¯\_(ツ)_/¯
+    </a>
 
 </div>
