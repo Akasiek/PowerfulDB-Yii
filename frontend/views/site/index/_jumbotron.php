4da06e83f923a18e0227253c0a47fd5413dd5db4
Kamil Pomykała <37307597+Akasiek@users.noreply.github.com>
2022-07-22 16:37:00 +0200

    Update _jumbotron.php

--- frontend/views/site/index/_jumbotron.php
+++ frontend/views/site/index/_jumbotron.php
@@ -5,7 +5,8 @@ use yii\helpers\Html;
 ?>
 
 <div class="flex relative items-center justify-center w-full h-[650px] lg:h-[750px] bg-cover">
-    <img src="<?= Yii::getAlias('@web/resources/images/jumbotron_bg.jpg') ?>" class="z-0 absolute object-cover object-center h-full w-full" alt="Jumbotron background image">
+    <img src="<?= Yii::getAlias('@web/resources/images/jumbotron_bg.jpg') ?>"
+         class="z-0 absolute object-cover object-center h-full w-full" alt="Jumbotron background image">
     <div class="z-10 flex items-center justify-center flex-col gap-3">
         <?php echo Html::img(
             '@web/resources/logo/logo.svg',
@@ -15,7 +16,7 @@ use yii\helpers\Html;
             ]
         ) ?>
         <p class="font-serif text-main-light px-4 text-sm md:text-lg lg:text-xl">
-            Powerful Database of Rock and Metal Music
+            Powerful Database of Music Information
         </p>
     </div>
 </div>
